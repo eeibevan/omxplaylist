d6cf7ba5a5a0e5e11832f5f5b552ac267cc1222f
Evan Black <me@evanpblack.com>
2016-12-20 21:09:56 -0500

    Add Option Support

--- playlist.sh
+++ playlist.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+opts=`getopt --quiet --options H,r,s: --long hdmi,resume,start: -- "$@"`
+if [ $? -ne 0 ]; then
+    echo "Error: Bad Argument" 1>&2;
+    exit 1;
+fi
+eval set -- "$opts";
+
+
+while true; do
+    case "$1" in
+        -H|hdmi)
+            echo "HDMI";
+            shift;
+            ;;
+        -r|--resume)
+            echo "Resume";
+            shift;
+            ;;
+        -s|--start)
+            echo "Start and $2";
+            shift 2;
+            ;;
+        --)
+            shift;
+            break;
+            ;;
+    esac
+done
+
